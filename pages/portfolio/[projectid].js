74d489632fdc4a389dec9422c4006350ba53ba72
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-11 06:23:16 +0300

    adding dynamic paths & routes

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PortfolioProjectPage = () => {
+    return (
+        <div>
+            <h2>The PortfolioProjectPage</h2>
+        </div>
+    );
+};
+
+export default PortfolioProjectPage;
