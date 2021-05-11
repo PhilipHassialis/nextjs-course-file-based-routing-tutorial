194b3524fefe71b1c757e22022365318a55e3f1b
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-11 07:09:19 +0300

    navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,20 @@ const ClientsProjectsPage = () => {
     const router = useRouter();
     console.log(router.query);
 
+    const buttonHandler = () => {
+        router.push({
+            pathname: "/clients/[id]/[clientprojectid]",
+            query: {
+                id: router.query.id,
+                clientprojectid: "first-project",
+            },
+        });
+    };
+
     return (
         <div>
             <h4>The projects of a given client with id {router.query.id}</h4>
+            <button onClick={buttonHandler}>Load Project A</button>
         </div>
     );
 };
