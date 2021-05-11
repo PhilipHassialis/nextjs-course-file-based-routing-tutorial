3febe1899b65a03de767306e6bac1f613ba09a4c
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-11 06:27:02 +0300

    extracting dynamic path segment data (dynamic routes)

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const PortfolioProjectPage = () => {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+
     return (
         <div>
             <h2>The PortfolioProjectPage</h2>
